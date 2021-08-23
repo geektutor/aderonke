974116124e989ceb9ae8f9bff67e5562fdc14624
geektutor <sodiq.akinjobi@gmail.com>
2021-08-23 20:43:06 +0100

    Changed server

--- frontity.settings.js
+++ frontity.settings.js
@@ -45,7 +45,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org"
+          "url": "https://api.geektutor.xyz"
         }
       }
     },
