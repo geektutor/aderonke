3f375b148acad94096068092d655140f317f314c
geektutor <sodiq.akinjobi@gmail.com>
2021-08-23 20:30:11 +0100

    First Push

---
+++ frontity.settings.js
@@ -0,0 +1,57 @@
+const settings = {
+  "name": "aderonke",
+  "state": {
+    "frontity": {
+      "url": "https://api.geektutor.xyz",
+      "title": "Geektutor's Website",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://test.frontity.org"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
