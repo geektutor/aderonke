c69b1c25c913e209514bcddae20f1ec4d2a176ef
geektutor <sodiq.akinjobi@gmail.com>
2021-08-25 19:02:10 +0100

    Added Rukky theme

---
+++ packages/rukky/src/index.js
@@ -0,0 +1,16 @@
+import Root from "./components"
+
+const rukky = {
+  name: "rukky",
+  roots: {
+    theme: Root,
+  },
+  state: {
+    theme: {}
+  },
+  actions: {
+    theme: {}
+  },
+}
+
+export default rukky
